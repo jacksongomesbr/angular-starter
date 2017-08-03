3c88f4495b2f3ce223414f155e7b146664c3c23b
Jackson Gomes <jgomes@ceulp.edu.br>
2017-08-03 18:43:38 -0300

    Atualizações das versões das dependências

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+import '../assets/css/style.css';
+
+@Component({
+    selector: 'my-app',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+    constructor() {
+    }
+
+    ngOnInit(): void {
+    }
+}
