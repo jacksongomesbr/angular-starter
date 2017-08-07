bbdffbc3ab6e6f2c6cb016e862f66fa2df9ee382
Jackson Gomes <jgomes@ceulp.edu.br>
2017-08-07 12:31:15 -0300

    Atualização na configuração do Travis-ci e nos testes

--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -5,7 +5,7 @@ export class MyAppPage {
     return browser.get('/');
   }
 
-  getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+  getPageTitle() {
+    return element(by.css('h1')).getText();
   }
 }
