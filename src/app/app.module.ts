222f8fab1c7543d54175d74936edaa81c16522dc
Jackson Gomes <jgomes@ceulp.edu.br>
2017-08-24 11:28:27 -0300

    Inclusão do FormsModule no módulo principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-import {ReactiveFormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
 import {AppComponent} from './app.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
@@ -9,6 +9,7 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
     imports: [
         BrowserModule,
         NgbModule.forRoot(),
+        FormsModule,
         ReactiveFormsModule,
         HttpModule
     ],
