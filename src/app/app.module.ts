16cb2d4d90c64d8a6b9bd2dcbf2a407d08ce65e9
Jackson Gomes <jgomes@ceulp.edu.br>
2017-10-26 00:14:53 -0300

    Atualização para versão 4.4.6 do Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
 import {AppComponent} from './app.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
@@ -10,7 +10,6 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
         BrowserModule,
         NgbModule.forRoot(),
         FormsModule,
-        ReactiveFormsModule,
         HttpModule
     ],
     declarations: [
